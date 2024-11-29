f2c6bfd33ad1bda3226f02966c9c1c51d3962484
j0th4r <124442886+j0th4r@users.noreply.github.com>
2024-11-29 22:15:16 +0800

    Add counter components for step 3 of listing creation

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -11,6 +11,7 @@ import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
+import Counter from "../inputs/Counter";
 
 enum STEPS {
   CATEGORY = 0,
@@ -40,8 +41,8 @@ const RentModal = () => {
       category: '',
       location: null,
       guestCount: 1,
-      roomCount: 1,
-      bathroomCount: 1,
+      tableCount: 1,
+      seatingCapacity: 1,
       imageSrc: '',
       price: 1,
       title: '',
@@ -51,6 +52,9 @@ const RentModal = () => {
 
   const category = watch('category');
   const location = watch('location');
+  const guestCount = watch('guestCount');
+  const tableCount = watch('tableCount');
+  const seatingCapacity = watch('seatingCapacity');
 
   const Map = useMemo(() => dynamic(() => import('../Map'), {
     ssr: false
@@ -132,6 +136,37 @@ const RentModal = () => {
         />
         <Map 
           center={location?.latlng}
+        />
+      </div>
+    )
+  }
+
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading 
+          title="Tell us more about your restaurant"
+          subtitle="Help guests know what to expect!"
+        />
+        <Counter
+          title="Guests"
+          subtitle="How many guests do you allow?"
+          value={guestCount}
+          onChange={(value) => setCustomValue('guestCount', value)}
+        />
+        <hr />
+        <Counter
+          title="Tables"
+          subtitle="How many tables do you have?"
+          value={tableCount}
+          onChange={(value) => setCustomValue('tableCount', value)}
+        />
+        <hr />
+        <Counter
+          title="Seating Capacity"
+          subtitle="Total number of seats available"
+          value={seatingCapacity}
+          onChange={(value) => setCustomValue('seatingCapacity', value)}
         />
       </div>
     )
